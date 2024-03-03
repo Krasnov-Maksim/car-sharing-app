5cb678563e3d5d895202f4219fbcd2c8797d94e0
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-03-03 13:15:22 +0200

    [API] Add permissions to Car Controller

--- src/main/java/mate/academy/carsharing/controller/CarController.java
+++ src/main/java/mate/academy/carsharing/controller/CarController.java
@@ -10,6 +10,8 @@ import mate.academy.carsharing.dto.CarResponseDto;
 import mate.academy.carsharing.dto.CreateCarRequestDto;
 import mate.academy.carsharing.service.CarService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,18 +29,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarController {
     private final CarService carService;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Operation(summary = "Create a new car", description = "Create a new car")
     @PostMapping
     public CarResponseDto create(@RequestBody @Valid CreateCarRequestDto carDto) {
         return carService.save(carDto);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Operation(summary = "Delete a car by id", description = "Delete car with specified id")
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
         carService.deleteById(id);
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Operation(summary = "Update a car by id", description = "Update car with specified id")
     @PutMapping("/{id}")
     public CarResponseDto updateById(@PathVariable Long id,
@@ -45,12 +54,16 @@ public class CarController {
         return carService.updateById(id, requestDto);
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER', 'ROLE_ANONYMOUS')")
     @Operation(summary = "Get a car by id", description = "Get a car with specified id")
     @GetMapping("/{id}")
     public CarResponseDto getById(@PathVariable Long id) {
         return carService.getById(id);
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("permitAll()")
     @Operation(summary = "Get all cars", description = "Get a list of all cars")
     @Parameter(name = "page", description = "page index, default value = 0")
     @Parameter(name = "size", description = "elements per page, default value = 20")
