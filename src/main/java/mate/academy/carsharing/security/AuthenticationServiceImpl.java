f2f3171f7b94c2c0286fe8665bc520f41506501c
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-03-03 13:57:09 +0200

    [API] Add AuthenticationController, AuthenticationService

---
+++ src/main/java/mate/academy/carsharing/security/AuthenticationServiceImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.carsharing.security;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.carsharing.dto.user.UserLoginRequestDto;
+import mate.academy.carsharing.dto.user.UserLoginResponseDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public UserLoginResponseDto authentication(UserLoginRequestDto requestDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.email(), requestDto.password())
+        );
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
