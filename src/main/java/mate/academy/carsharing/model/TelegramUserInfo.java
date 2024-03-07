d0be682cc42028d35e817a21af7e263a7710fce8
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-03-07 15:28:32 +0200

    Add 'TelegramUserInfo' entity and table 'telegram_user_info'

---
+++ src/main/java/mate/academy/carsharing/model/TelegramUserInfo.java
@@ -0,0 +1,31 @@
+package mate.academy.carsharing.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "telegram_user_info")
+@SQLDelete(sql = "UPDATE telegram_user_info SET deleted = true WHERE id = ?")
+@SQLRestriction("deleted = false")
+@Data
+@NoArgsConstructor
+public class TelegramUserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long chatId;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
